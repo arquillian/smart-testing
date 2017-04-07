1ee4abffee5d3e3af3e48a22dddfb4feba13bf52
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-04-07 16:36:48 +0200

    Stub test

---
+++ strategies/changed/src/test/java/org/arquillian/smart/testing/vcs/git/NewFilesDetectorTest.java
@@ -0,0 +1,12 @@
+package org.arquillian.smart.testing.vcs.git;
+
+import org.junit.Test;
+
+public class NewFilesDetectorTest {
+
+    @Test
+    public void should_find_all_new_classes() throws Exception {
+
+    }
+
+}
