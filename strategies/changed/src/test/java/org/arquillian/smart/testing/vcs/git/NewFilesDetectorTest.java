04d04fec11b5e3d263b675177d68b052af2641d5
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-04-07 19:19:07 +0200

    Moves repository unpacking to its own class

--- strategies/changed/src/test/java/org/arquillian/smart/testing/vcs/git/NewFilesDetectorTest.java
+++ strategies/changed/src/test/java/org/arquillian/smart/testing/vcs/git/NewFilesDetectorTest.java
@@ -1,14 +1,12 @@
 package org.arquillian.smart.testing.vcs.git;
 
 import java.net.URL;
-import org.arquillian.spacelift.Spacelift;
-import org.arquillian.spacelift.process.CommandBuilder;
-import org.arquillian.spacelift.task.os.CommandTool;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import static org.arquillian.smart.testing.vcs.git.GitRepositoryUnpacker.unpackRepository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NewFilesDetectorTest {
@@ -19,12 +17,9 @@ public class NewFilesDetectorTest {
     @BeforeClass
     public static void unpack_repo() {
         final URL repoBundle = Thread.currentThread().getContextClassLoader().getResource("repo.bundle");
-        Spacelift.task(CommandTool.class)
-            .command(new CommandBuilder("git")
-                .parameters("clone", repoBundle.getFile(), "-b", "master",
-                    gitFolder.getRoot().getAbsolutePath()).build())
-            .execute().await();
+        unpackRepository(gitFolder.getRoot().getAbsolutePath(), repoBundle.getFile());
     }
+
 
     @Test
     public void should_find_all_new_classes_in_the_range_of_commits() throws Exception {
