13f18eedea02cf09aa2082c6a25afa3a55f4e502
Alex Soto <asotobu@gmail.com>
2017-04-07 19:11:38 +0200

    Removes unused import

--- core/src/main/java/org/arquillian/smart/testing/Files.java
+++ core/src/main/java/org/arquillian/smart/testing/Files.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Formatter;
 
 public class Files {
 
