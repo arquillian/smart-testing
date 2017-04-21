6a09fcb08abc808373c7ce9a04fde73e0ba961c8
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-04-22 00:54:02 +0200

    Renames ftests

--- ftest/src/test/java/org/arquillian/smart/testing/ftest/FirstTest.java
+++ ftest/src/test/java/org/arquillian/smart/testing/ftest/FirstTest.java
@@ -3,11 +3,11 @@ package org.arquillian.smart.testing.ftest;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class FirstJUnitTest {
+public class FirstTest {
 
     @Test
     public void should_print_text_of_first_test() {
-        System.out.println("This is first junit test");
+        System.out.println(">>>>> This is first junit test");
         Assert.assertTrue(true);
     }
 }
--- ftest/src/test/java/org/arquillian/smart/testing/ftest/SecondTest.java
+++ ftest/src/test/java/org/arquillian/smart/testing/ftest/SecondTest.java
@@ -3,11 +3,11 @@ package org.arquillian.smart.testing.ftest;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class SecondJUnitTest {
+public class SecondTest {
 
     @Test
     public void should_print_text_of_second_test() {
-        System.out.println("This is second junit test");
+        System.out.println(">>>>> This is second junit test");
         Assert.assertTrue(true);
     }
 }
