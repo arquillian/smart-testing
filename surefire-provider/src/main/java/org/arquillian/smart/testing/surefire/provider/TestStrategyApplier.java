f8131e92589135d7a7f468020828c4a4d0a90c85
Alex Soto <asotobu@gmail.com>
2017-05-24 13:51:49 +0200

    feat(#13): Adds possibility to configure order vs smart execution (#14)
    Closes #13

--- surefire-provider/src/main/java/org/arquillian/smart/testing/surefire/provider/TestStrategyApplier.java
+++ surefire-provider/src/main/java/org/arquillian/smart/testing/surefire/provider/TestStrategyApplier.java
@@ -9,17 +9,43 @@ import org.arquillian.smart.testing.spi.TestExecutionPlanner;
 
 class TestStrategyApplier {
 
+    static final String USAGE = "usage";
+
     private final TestExecutionPlannerLoader testExecutionPlannerLoader;
     private TestsToRun testsToRun;
+    private ProviderParametersParser paramsProvider;
 
-    TestStrategyApplier(TestsToRun testsToRun, TestExecutionPlannerLoader testExecutionPlannerLoader) {
+    TestStrategyApplier(TestsToRun testsToRun, ProviderParametersParser paramsProvider, TestExecutionPlannerLoader testExecutionPlannerLoader) {
         this.testsToRun = testsToRun;
         this.testExecutionPlannerLoader = testExecutionPlannerLoader;
+        this.paramsProvider = paramsProvider;
     }
 
-    TestsToRun apply(List<String> orderStrategy) {
+    TestsToRun apply(List<String> strategies) {
+        final Set<Class<?>> smartTests = getTestsByRunningStrategies(strategies);
+
+        if (isUsageSet() && isSelectingMode()) {
+            return new TestsToRun(smartTests);
+        } else {
+            final Set<Class<?>> orderedTests = new LinkedHashSet<>(smartTests);
+            testsToRun.iterator().forEachRemaining(orderedTests::add);
+
+            return new TestsToRun(orderedTests);
+        }
+
+    }
+
+    private boolean isSelectingMode() {
+        return RunMode.SELECTING.name().equalsIgnoreCase(paramsProvider.getProperty(USAGE));
+    }
+
+    private boolean isUsageSet() {
+        return paramsProvider.containsProperty(USAGE);
+    }
+
+    private Set<Class<?>> getTestsByRunningStrategies(List<String> strategies) {
         final Set<Class<?>> orderedTests = new LinkedHashSet<>();
-        for (final String strategy : orderStrategy) {
+        for (final String strategy : strategies) {
 
             final TestExecutionPlanner plannerForStrategy = testExecutionPlannerLoader.getPlannerForStrategy(strategy);
             final List<? extends Class<?>> tests = plannerForStrategy.getTests().stream().map(testClass -> {
@@ -31,8 +57,6 @@ class TestStrategyApplier {
             }).collect(Collectors.toList());
             orderedTests.addAll(tests);
         }
-        testsToRun = new TestsToRun(orderedTests);
-        return testsToRun;
+        return orderedTests;
     }
-
 }
---
+++ surefire-provider/src/test/java/org/arquillian/smart/testing/surefire/provider/TestStrategyApplierTest.java
@@ -0,0 +1,93 @@
+package org.arquillian.smart.testing.surefire.provider;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.apache.maven.surefire.util.TestsToRun;
+import org.arquillian.smart.testing.spi.TestExecutionPlanner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestStrategyApplierTest {
+
+    @Mock
+    private ProviderParametersParser providerParametersParser;
+
+    @Mock
+    private TestExecutionPlannerLoader testExecutionPlannerLoader;
+
+    @Mock
+    private TestExecutionPlanner testExecutionPlanner;
+
+    @Test
+    public void should_return_tests_only_from_strategies_when_filtering_mode_configured() {
+
+        // given
+
+        final Set<Class<?>> defaultTestsToRun = new HashSet<>();
+        defaultTestsToRun.add(ProviderParameterParserTest.class);
+
+        final TestsToRun testsToRun = new TestsToRun(defaultTestsToRun);
+
+        when(providerParametersParser.containsProperty(TestStrategyApplier.USAGE)).thenReturn(true);
+        when(providerParametersParser.getProperty(TestStrategyApplier.USAGE)).thenReturn(RunMode.SELECTING.name().toLowerCase());
+        when(testExecutionPlannerLoader.getPlannerForStrategy("static")).thenReturn(testExecutionPlanner);
+
+        final Set<String> strategyTests = new LinkedHashSet<>();
+        strategyTests.add(TestExecutionPlannerLoaderTest.class.getName());
+
+        when(testExecutionPlanner.getTests()).thenReturn(strategyTests);
+
+        // when
+
+        TestStrategyApplier testStrategyApplier = new TestStrategyApplier(testsToRun, providerParametersParser, testExecutionPlannerLoader);
+        final TestsToRun realTestPlanning = testStrategyApplier.apply(Arrays.asList("static"));
+
+        // then
+
+        assertThat(realTestPlanning.getLocatedClasses())
+            .hasSize(1)
+            .containsExactly(TestExecutionPlannerLoaderTest.class);
+
+    }
+
+    @Test
+    public void should_return_tests_ordered_by_default() {
+
+        // given
+
+        final Set<Class<?>> defaultTestsToRun = new LinkedHashSet<>();
+        defaultTestsToRun.add(ProviderParameterParserTest.class);
+        defaultTestsToRun.add(TestStrategyApplierTest.class);
+
+        final TestsToRun testsToRun = new TestsToRun(defaultTestsToRun);
+
+        when(providerParametersParser.containsProperty(TestStrategyApplier.USAGE)).thenReturn(false);
+        when(testExecutionPlannerLoader.getPlannerForStrategy("static")).thenReturn(testExecutionPlanner);
+
+        final Set<String> strategyTests = new LinkedHashSet<>();
+        strategyTests.add(TestExecutionPlannerLoaderTest.class.getName());
+
+        when(testExecutionPlanner.getTests()).thenReturn(strategyTests);
+
+        // when
+
+        TestStrategyApplier testStrategyApplier = new TestStrategyApplier(testsToRun, providerParametersParser, testExecutionPlannerLoader);
+        final TestsToRun realTestPlanning = testStrategyApplier.apply(Arrays.asList("static"));
+
+        // then
+
+        assertThat(realTestPlanning.getLocatedClasses())
+            .hasSize(3)
+            .containsExactly(TestExecutionPlannerLoaderTest.class, ProviderParameterParserTest.class, TestStrategyApplierTest.class);
+
+    }
+
+}
