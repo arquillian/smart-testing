b2dee4ed28e1a8c1a89ae6ab82773493222ead76
Dipak Pawar <dipakpawar231@gmail.com>
2017-06-12 13:27:34 +0530

    Adds support for git.last.commits system property. (#18)
    Closes #17

--- strategies/affected/src/main/java/org/arquillian/smart/testing/strategies/affected/AffectedChangesDetectorFactory.java
+++ strategies/affected/src/main/java/org/arquillian/smart/testing/strategies/affected/AffectedChangesDetectorFactory.java
@@ -2,10 +2,16 @@ package org.arquillian.smart.testing.strategies.affected;
 
 import java.io.File;
 import java.util.Set;
+import org.arquillian.smart.testing.GitRunnerProperties;
 import org.arquillian.smart.testing.spi.TestExecutionPlanner;
 import org.arquillian.smart.testing.spi.TestExecutionPlannerFactory;
 import org.arquillian.smart.testing.strategies.affected.detector.FileSystemTestClassDetector;
 import org.arquillian.smart.testing.vcs.git.AllChangedFilesDetector;
+
+import static org.arquillian.smart.testing.GitRunnerProperties.COMMIT;
+import static org.arquillian.smart.testing.GitRunnerProperties.HEAD;
+import static org.arquillian.smart.testing.GitRunnerProperties.PREVIOUS_COMMIT;
+import static org.arquillian.smart.testing.GitRunnerProperties.getPrevCommitDefaultValue;
 
 public class AffectedChangesDetectorFactory implements TestExecutionPlannerFactory {
 
@@ -23,9 +29,8 @@ public class AffectedChangesDetectorFactory implements TestExecutionPlannerFacto
     public TestExecutionPlanner create(File projectDir, String[] globPatterns) {
         // TODO logic of inspecting git changes should be some where common so it is not recalculated several times
         // TODO in fact there are at least two things to be put in a Context to be reused (git changes (main and test) and graph of dependencies between tests and main classes
-
-        final String previousCommit = System.getProperty("git.previous.commit", "HEAD");
-        final String commit = System.getProperty("git.commit", "HEAD");
+        final String previousCommit = System.getProperty(PREVIOUS_COMMIT, getPrevCommitDefaultValue());
+        final String commit = System.getProperty(COMMIT, HEAD);
 
         // For now we recalculate everything
         AllChangedFilesDetector allChangedFilesDetector = new AllChangedFilesDetector(projectDir, previousCommit, commit, "**/src/main/java/**/*.java");
--- strategies/changed/src/main/java/org/arquillian/smart/testing/vcs/git/ChangedFilesDetectorFactory.java
+++ strategies/changed/src/main/java/org/arquillian/smart/testing/vcs/git/ChangedFilesDetectorFactory.java
@@ -1,8 +1,15 @@
 package org.arquillian.smart.testing.vcs.git;
 
 import java.io.File;
+import org.arquillian.smart.testing.GitRunnerProperties;
 import org.arquillian.smart.testing.spi.TestExecutionPlanner;
 import org.arquillian.smart.testing.spi.TestExecutionPlannerFactory;
+import org.eclipse.jgit.api.Git;
+
+import static org.arquillian.smart.testing.GitRunnerProperties.COMMIT;
+import static org.arquillian.smart.testing.GitRunnerProperties.HEAD;
+import static org.arquillian.smart.testing.GitRunnerProperties.PREVIOUS_COMMIT;
+import static org.arquillian.smart.testing.GitRunnerProperties.getPrevCommitDefaultValue;
 
 public class ChangedFilesDetectorFactory implements TestExecutionPlannerFactory {
 
@@ -18,9 +25,9 @@ public class ChangedFilesDetectorFactory implements TestExecutionPlannerFactory 
 
     @Override
     public TestExecutionPlanner create(File projectDir, String[] globPatterns) {
-        final String previousCommit = System.getProperty("git.previous.commit", "HEAD");
-        final String commit = System.getProperty("git.commit", "HEAD");
+        final String previousCommit = System.getProperty(PREVIOUS_COMMIT, getPrevCommitDefaultValue());
+        final String commit = System.getProperty(COMMIT, HEAD);
+
         return new ChangedFilesDetector(projectDir, previousCommit, commit, globPatterns);
     }
-
 }
