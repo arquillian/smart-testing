e1b84c400431e3a34f4e4971a6979ddbd03cebbf
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-07-05 22:37:22 +0200

    feat(#32): adds testes for "new" tests execution

--- functional-tests/test-bed/src/main/java/org/arquillian/smart/testing/ftest/testbed/configuration/Criteria.java
+++ functional-tests/test-bed/src/main/java/org/arquillian/smart/testing/ftest/testbed/configuration/Criteria.java
@@ -5,13 +5,20 @@ import org.apache.maven.model.Dependency;
 import static org.arquillian.smart.testing.ftest.testbed.project.Project.SMART_TESTING_VERSION;
 
 public enum Criteria {
-    AFFECTED;
+    AFFECTED("affected"),
+    NEW("changed");
+
+    private final String dependencySuffix;
+
+    Criteria(String dependencySuffix) {
+        this.dependencySuffix = dependencySuffix;
+    }
 
     public Dependency getMavenDependency() {
         Dependency mavenDependency = new Dependency();
         mavenDependency.setGroupId("org.arquillian.smart.testing");
         mavenDependency.setVersion(SMART_TESTING_VERSION);
-        mavenDependency.setArtifactId("smart-testing-strategy-" + this.name().toLowerCase());
+        mavenDependency.setArtifactId("smart-testing-strategy-" + dependencySuffix);
         return mavenDependency;
     }
 }
---
+++ functional-tests/test-bed/src/test/java/org/arquillian/smart/testing/ftest/newtests/LocalChangesNewTestsSelectionExecutionFunctionalTest.java
@@ -0,0 +1,55 @@
+package org.arquillian.smart.testing.ftest.newtests;
+
+import java.util.List;
+import org.arquillian.smart.testing.ftest.TestBedTemplate;
+import org.arquillian.smart.testing.ftest.testbed.testresults.TestResult;
+import org.junit.Test;
+
+import static org.arquillian.smart.testing.ftest.testbed.configuration.Criteria.NEW;
+import static org.arquillian.smart.testing.ftest.testbed.configuration.Mode.SELECTING;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LocalChangesNewTestsSelectionExecutionFunctionalTest extends TestBedTemplate {
+
+    @Test
+    public void should_only_execute_new_tests_related_to_single_local_change() throws Exception {
+        // given
+        project.configureSmartTesting()
+                    .executionOrder(NEW)
+                    .inMode(SELECTING)
+               .enable();
+
+        final List<TestResult> expectedTestResults = project
+            .applyAsLocalChanges("Adds new unit test");
+
+        // when
+        final List<TestResult> actualTestResults = project.build();
+
+        // then
+        assertThat(actualTestResults).containsAll(expectedTestResults).hasSameSizeAs(expectedTestResults);
+    }
+
+    @Test
+    public void should_only_execute_new_tests_when_multiple_local_changes_applied() throws Exception {
+        // given
+        project.configureSmartTesting()
+                    .executionOrder(NEW)
+                    .inMode(SELECTING)
+            .enable();
+
+        // we ignore expected tests results for this commits, as they are changes to existing ones,
+        // but still want to apply the changes
+        project.applyAsLocalChanges("Single method body modification - sysout",
+            "Inlined variable in a method");
+
+        final List<TestResult> expectedTestResults = project
+            .applyAsLocalChanges("Adds new unit test");
+
+        // when
+        final List<TestResult> actualTestResults = project.build();
+
+        // then
+        assertThat(actualTestResults).containsAll(expectedTestResults).hasSameSizeAs(expectedTestResults);
+    }
+
+}
