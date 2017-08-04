cf521632ca76d30d62e78410a1055a5d6d379f46
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-08-04 11:00:05 +0200

    chore: renames central mvn extension class

--- mvn-extension/src/main/java/org/arquillian/smart/testing/mvn/ext/SmartTestingMavenConfigurer.java
+++ mvn-extension/src/main/java/org/arquillian/smart/testing/mvn/ext/SmartTestingMavenConfigurer.java
@@ -17,11 +17,13 @@ import org.codehaus.plexus.component.annotations.Component;
 
 import static java.util.stream.StreamSupport.stream;
 
-@Component(role = AbstractMavenLifecycleParticipant.class, description = "TODO fill it in later", // TODO
-    hint = "smart-testing-optimizer")
-class PreBuildTestOptimizer extends AbstractMavenLifecycleParticipant {
+@Component(role = AbstractMavenLifecycleParticipant.class,
+    description = "Entry point to install and manage Smart-Testing extension. Takes care of adding needed dependencies and "
+        + "configures it on the fly.", // TODO
+    hint = "smart-testing")
+class SmartTestingMavenConfigurer extends AbstractMavenLifecycleParticipant {
 
-    private static final Logger logger = Logger.getLogger(PreBuildTestOptimizer.class);
+    private static final Logger logger = Logger.getLogger(SmartTestingMavenConfigurer.class);
 
     private final ChangeStorage changeStorage = new LocalChangeStorage(".");
 
@@ -66,12 +68,10 @@ class PreBuildTestOptimizer extends AbstractMavenLifecycleParticipant {
             .flatMap(Collection::stream)
             .collect(Collectors.toSet());
 
-        changeStorage.store(changes); // FIXME DI? dunno
+        changeStorage.store(changes);
     }
 
     private void configureExtension(MavenSession session, Configuration configuration) {
-        // TODO do we want to inject MavenProjectConfigurator instead?
-        // FIXME NPE when we don't specify properties from CLI
         final MavenProjectConfigurator mavenProjectConfigurator = new MavenProjectConfigurator(configuration);
         session.getAllProjects().forEach(mavenProject -> {
             final Model model = mavenProject.getModel();
