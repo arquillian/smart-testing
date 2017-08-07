a7e631ed5dc8ec5a490a146f837aee3fc2c9b0d4
Alex Soto <asotobu@gmail.com>
2017-08-07 19:07:11 +0200

    Adds Ignore to test tests

--- strategies/affected/src/test/java/org/arquillian/smart/testing/strategies/affected/fakeproject/test/CTest.java
+++ strategies/affected/src/test/java/org/arquillian/smart/testing/strategies/affected/fakeproject/test/CTest.java
@@ -1,8 +1,10 @@
 package org.arquillian.smart.testing.strategies.affected.fakeproject.test;
 
 import org.arquillian.smart.testing.strategies.affected.fakeproject.main.C;
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class CTest {
 
     private C c;
