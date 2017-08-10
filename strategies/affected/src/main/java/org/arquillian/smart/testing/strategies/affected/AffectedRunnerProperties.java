f4ed0f3aca7a962d5d885f448c0b3e80402fe95c
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-08-10 12:44:16 +0200

    chore: small cleanup in AffectedRunnerProperties

--- strategies/affected/src/main/java/org/arquillian/smart/testing/strategies/affected/AffectedRunnerProperties.java
+++ strategies/affected/src/main/java/org/arquillian/smart/testing/strategies/affected/AffectedRunnerProperties.java
@@ -1,18 +1,18 @@
 package org.arquillian.smart.testing.strategies.affected;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class AffectedRunnerProperties {
+class AffectedRunnerProperties {
 
-    public static final String SMART_TESTING_AFFECTED_EXCLUSIONS = "smart.testing.affected.exclusions";
-    public static final String SMART_TESTING_AFFECTED_INCLUSIONS = "smart.testing.affected.inclusions";
-    public static final String SMART_TESTING_AFFECTED_CONFIG = "smart.testing.affected.config";
+    private static final String SMART_TESTING_AFFECTED_CONFIG = "smart.testing.affected.config";
+
+    static final String SMART_TESTING_AFFECTED_EXCLUSIONS = "smart.testing.affected.exclusions";
+    static final String SMART_TESTING_AFFECTED_INCLUSIONS = "smart.testing.affected.inclusions";
     static final String INCLUSIONS = "inclusions";
     static final String EXCLUSIONS = "exclusions";
 
@@ -31,18 +31,18 @@ public class AffectedRunnerProperties {
         try {
             properties.load(Files.newBufferedReader(Paths.get(location)));
         } catch (IOException e) {
-            throw new IllegalStateException(e);
+            throw new UncheckedIOException(e);
         }
     }
 
-    public static final String getSmartTestingAffectedExclusions() {
+    static String getSmartTestingAffectedExclusions() {
         String exclusions = System.getProperty(SMART_TESTING_AFFECTED_EXCLUSIONS);
-        String excusionsFromFile = properties.getProperty(EXCLUSIONS);
+        String exclusionsFromFile = properties.getProperty(EXCLUSIONS);
 
-        return resolve(exclusions, excusionsFromFile);
+        return resolve(exclusions, exclusionsFromFile);
     }
 
-    public static final String getSmartTestingAffectedInclusions() {
+    static String getSmartTestingAffectedInclusions() {
         String inclusions = System.getProperty(SMART_TESTING_AFFECTED_INCLUSIONS);
         String inclusionsFromFile = properties.getProperty(INCLUSIONS);
 
