df43f8c435bb43a845f7ae863d2b8e57aa5a5609
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-09-20 12:12:44 +0200

    feat(#170): suggests closest matching strategy when mistyped (#172)

---
+++ mvn-extension/src/test/java/org/arquillian/smart/testing/mvn/ext/dependencies/StringSimilarityCalculatorTest.java
@@ -0,0 +1,38 @@
+package org.arquillian.smart.testing.mvn.ext.dependencies;
+
+import java.util.Arrays;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringSimilarityCalculatorTest {
+
+    @Test
+    public void should_find_exact_match() throws Exception {
+        // given
+        final String sample = "sample";
+
+        // when
+        final String closestMatch =
+            new StringSimilarityCalculator().findClosestMatch(sample, Arrays.asList("smaple", "sapmle", "", "sample"));
+
+        // then
+        assertThat(closestMatch).isEqualTo(sample);
+    }
+
+    @Test
+    public void should_find_match_for_misspelled_strategy() throws Exception {
+        // given
+        final String misspelled = "cahnged";
+        final String expectedWord = "changed";
+
+        // when
+        final String closestMatch =
+            new StringSimilarityCalculator().findClosestMatch(misspelled, Arrays.asList("failed", "affected", "changed", "new"));
+
+        // then
+        assertThat(closestMatch).isEqualTo(expectedWord);
+    }
+
+
+}
