e3a57aad320be5c1b97cf19f46a536125ae92ed3
Alex Soto <asotobu@gmail.com>
2017-12-12 15:41:12 +0100

    docs: Add docs for WatchFile annotation

--- strategies/affected/src/test/java/org/arquillian/smart/testing/strategies/affected/fakeproject/test/XTest.java
+++ strategies/affected/src/test/java/org/arquillian/smart/testing/strategies/affected/fakeproject/test/XTest.java
@@ -5,9 +5,10 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 @Ignore("Test ignored because it is used internally")
+//tag::docs[]
 @WatchFile("src/main/resources/META-INF/persistence.xml")
 public class XTest {
-
+//end::docs[]
     @Test
     public void dao_test() {
         System.out.println("JPA Test");
