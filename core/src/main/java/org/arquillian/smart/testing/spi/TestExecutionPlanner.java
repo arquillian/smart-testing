03c4f8e629eeda810ac5c6e97f699cafb39493bb
Matous Jobanek <mjobanek@redhat.com>
2017-12-12 07:12:28 +0100

    added reference to the documentation

--- core/src/main/java/org/arquillian/smart/testing/spi/TestExecutionPlanner.java
+++ core/src/main/java/org/arquillian/smart/testing/spi/TestExecutionPlanner.java
@@ -5,9 +5,11 @@ import org.arquillian.smart.testing.TestSelection;
 
 public interface TestExecutionPlanner {
 
+    //tag::documentation[]
     Collection<TestSelection> selectTestsFromNames(Iterable<String> testsToRun);
 
     Collection<TestSelection> selectTestsFromClasses(Iterable<Class<?>> testsToRun);
+    //end::documentation[]
 
     String getName();
 
