607ebb475542ca91948baf27426d8a7149fca29c
spriadka <spriadka@github.com>
2018-02-21 10:49:25 +0100

    categorized-strategy-exclusions

--- strategies/categorized/src/main/java/org/arquillian/smart/testing/strategies/categorized/AbstractParser.java
+++ strategies/categorized/src/main/java/org/arquillian/smart/testing/strategies/categorized/AbstractParser.java
@@ -2,9 +2,15 @@ package org.arquillian.smart.testing.strategies.categorized;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
 import java.util.stream.Collectors;
+
+import java.util.stream.Stream;
 import org.arquillian.smart.testing.TestSelection;
 
 import static org.arquillian.smart.testing.strategies.categorized.CategorizedTestsDetector.CATEGORIZED;
@@ -13,67 +19,71 @@ public abstract class AbstractParser {
 
     private final CategorizedConfiguration strategyConfig;
     private final List<String> specifiedCategories;
+    private final List<String> excludedCategories;
 
     AbstractParser(CategorizedConfiguration strategyConfig) {
         this.strategyConfig = strategyConfig;
         specifiedCategories = Arrays.stream(strategyConfig.getCategories())
             .map(this::changeIfNonCaseSensitive)
             .collect(Collectors.toList());
+        excludedCategories = Arrays.stream(strategyConfig.getExcludedCategories())
+            .map(this::changeIfNonCaseSensitive)
+            .collect(Collectors.toList());
     }
 
-    TestSelection getTestSelectionIfMatched(Class<?> clazz){
-        List<String> classCategories = findCategories(clazz.getAnnotations());
-        if (strategyConfig.isMethods() && classCategories.isEmpty()) {
+    TestSelection getTestSelectionIfMatched(Class<?> clazz) {
+        if (strategyConfig.isMethods()) {
             return getSelectionWithMethods(clazz);
-        } else {
-            if (containsCorrectCategoriesMatchingReversed(classCategories)) {
-                return new TestSelection(clazz.getName(), CATEGORIZED);
-            }
         }
-        return TestSelection.NOT_MATCHED;
+        return shouldBeIncluded(findCategories(clazz.getAnnotations())) ? new TestSelection(clazz.getName(), CATEGORIZED)
+            : TestSelection.NOT_MATCHED;
     }
 
-    private TestSelection getSelectionWithMethods(Class<?> clazz){
-        List<Method> testMethods = Arrays.stream(clazz.getDeclaredMethods())
-            .filter(this::isTestMethod)
-            .collect(Collectors.toList());
-
-        List<String> selectedMethods = testMethods.stream()
-            .filter(method -> containsCorrectCategoriesMatchingReversed(findCategories(method.getAnnotations())))
-            .map(Method::getName)
-            .collect(Collectors.toList());
-
-        if (!selectedMethods.isEmpty()) {
-            if (selectedMethods.size() != testMethods.size()) {
-                return new TestSelection(clazz.getName(), selectedMethods, CATEGORIZED);
-            } else {
-                return new TestSelection(clazz.getName(), CATEGORIZED);
-            }
-        }
-        return TestSelection.NOT_MATCHED;
-    }
-
-    private boolean containsCorrectCategoriesMatchingReversed(List<String> presentCategories) {
-        if (strategyConfig.isReversed()) {
-            return !containsCorrectCategories(presentCategories);
-        }
-        return containsCorrectCategories(presentCategories);
-    }
-
-    private boolean containsCorrectCategories(List<String> presentCategories) {
+    private boolean matchesCategories(Collection<String> presentCategories, Collection<String> categories) {
         if (presentCategories.isEmpty()) {
             return false;
         }
 
         final List<String> intersection = presentCategories.stream()
-            .filter(category -> this.isSpecified(category, specifiedCategories))
+            .filter(category -> isSpecified(category, new ArrayList<>(categories)))
             .collect(Collectors.toList());
+        return !intersection.isEmpty();
+    }
 
-        if (strategyConfig.isMatchAll() || specifiedCategories.isEmpty()) {
-            return intersection.size() == specifiedCategories.size();
-        } else {
-            return !intersection.isEmpty();
+    private boolean shouldBeIncluded(Collection<String> presentCategories) {
+        if (specifiedCategories.isEmpty()) {
+            return true;
         }
+        return matchesCategories(presentCategories, specifiedCategories);
+    }
+
+    private boolean areCategoriesIncluded(Collection<String> presentCategories) {
+        return shouldBeIncluded(presentCategories) && !matchesCategories(presentCategories, excludedCategories);
+    }
+
+    private TestSelection getSelectionWithMethods(Class<?> clazz) {
+        List<String> classLevelCategories = findCategories(clazz.getAnnotations());
+        List<CategorizedMethod> categorizedMethods = getTestMethods(clazz)
+            .stream()
+            .map(method -> new CategorizedMethod(method, classLevelCategories))
+            .collect(Collectors.toList());
+        List<String> applicableMethodNames = categorizedMethods.stream()
+            .filter(method -> areCategoriesIncluded(method.getCategories()))
+            .map(CategorizedMethod::getName)
+            .collect(Collectors.toList());
+        if (applicableMethodNames.size() == getTestMethods(clazz).size()) {
+            return new TestSelection(clazz.getName(), CATEGORIZED);
+        }
+        if (!applicableMethodNames.isEmpty()) {
+            return new TestSelection(clazz.getName(), applicableMethodNames, CATEGORIZED);
+        }
+        return TestSelection.NOT_MATCHED;
+    }
+
+    private List<Method> getTestMethods(Class<?> clazz) {
+        return Arrays.stream(clazz.getDeclaredMethods())
+            .filter(this::isTestMethod)
+            .collect(Collectors.toList());
     }
 
     protected String changeIfNonCaseSensitive(String category) {
@@ -81,7 +91,28 @@ public abstract class AbstractParser {
     }
 
     protected abstract List<String> findCategories(Annotation[] annotations);
+
     protected abstract boolean isSpecified(String category, List<String> specifiedCategories);
+
     protected abstract boolean isTestMethod(Method method);
 
+    private class CategorizedMethod {
+
+        private final String name;
+        private final Set<String> categories;
+
+        CategorizedMethod(Method method, Collection<String> classLevelCategories) {
+            this.name = method.getName();
+            this.categories = Stream.concat(classLevelCategories.stream(), findCategories(method.getAnnotations()).stream())
+                .collect(Collectors.toSet());
+        }
+
+        Set<String> getCategories() {
+            return categories;
+        }
+
+        String getName() {
+            return name;
+        }
+    }
 }
--- strategies/categorized/src/test/java/org/arquillian/smart/testing/strategies/categorized/CategorizedTestsDetectorTest.java
+++ strategies/categorized/src/test/java/org/arquillian/smart/testing/strategies/categorized/CategorizedTestsDetectorTest.java
@@ -2,24 +2,32 @@ package org.arquillian.smart.testing.strategies.categorized;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import org.arquillian.smart.testing.TestSelection;
 import org.arquillian.smart.testing.configuration.Configuration;
 import org.arquillian.smart.testing.configuration.ConfigurationLoader;
-import org.arquillian.smart.testing.strategies.categorized.project.categories.FirstAndSecondCategorizedClass;
-import org.arquillian.smart.testing.strategies.categorized.project.categories.FirstCategorizedClass;
 import org.arquillian.smart.testing.strategies.categorized.project.categories.FirstCategory;
-import org.arquillian.smart.testing.strategies.categorized.project.categories.NonCategorizedClass;
+import org.arquillian.smart.testing.strategies.categorized.project.categories.FirstCategorizedClass;
 import org.arquillian.smart.testing.strategies.categorized.project.categories.SecondCategory;
+import org.arquillian.smart.testing.strategies.categorized.project.categories.FirstAndSecondCategorizedClass;
+import org.arquillian.smart.testing.strategies.categorized.project.categories.ThirdCategory;
 import org.arquillian.smart.testing.strategies.categorized.project.categories.ThirdCategorizedClass;
+import org.arquillian.smart.testing.strategies.categorized.project.categories.NonCategorizedClass;
 import org.arquillian.smart.testing.strategies.categorized.project.categories.WithCategorizedMethodsClass;
+import org.arquillian.smart.testing.strategies.categorized.project.categories.Included;
+import org.arquillian.smart.testing.strategies.categorized.project.categories.Excluded;
+import org.arquillian.smart.testing.strategies.categorized.project.categories.WithExcludedMethodClass;
+import org.arquillian.smart.testing.strategies.categorized.project.categories.WithIncludedAndExcludedCategoriesOnMethodsClass;
 import org.arquillian.smart.testing.strategies.categorized.project.tags.FastTaggedClass;
-import org.arquillian.smart.testing.strategies.categorized.project.tags.FirstAndSecondTaggedClass;
 import org.arquillian.smart.testing.strategies.categorized.project.tags.FirstTaggedClass;
+import org.arquillian.smart.testing.strategies.categorized.project.tags.FirstAndSecondTaggedClass;
 import org.arquillian.smart.testing.strategies.categorized.project.tags.NonTaggedClass;
 import org.arquillian.smart.testing.strategies.categorized.project.tags.ThirdTaggedClass;
 import org.arquillian.smart.testing.strategies.categorized.project.tags.WithTaggedMethodsClass;
+import org.arquillian.smart.testing.strategies.categorized.project.tags.WithExcludedTagClass;
 import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -53,6 +61,34 @@ public class CategorizedTestsDetectorTest {
         categorizedConfig =
             (CategorizedConfiguration) config.getStrategyConfiguration(CATEGORIZED);
         config.getStrategiesConfiguration().add(categorizedConfig);
+    }
+
+    @Test
+    public void should_return_all_classes_and_methods_with_any_category_when_no_category_is_set() {
+        //given
+        categorizedConfig.setCategories(new String[0]);
+
+        //when
+        Collection<TestSelection> testSelection =
+            new CategorizedTestsDetector(config).selectTestsFromClasses(classesToProcess);
+
+        //then
+        assertThat(testSelection).containsTestClassSelectionsExactlyInAnyOrder(
+            classesToProcess.stream().map(this::getTestSelection).toArray(TestSelection[]::new));
+    }
+
+    @Test
+    public void should_exclude_classes_with_matching_category_when_no_included_category_is_set() {
+        //given
+        categorizedConfig.setCategories(new String[0]);
+        categorizedConfig.setExcludedCategories(new String[] {SecondCategory.class.getSimpleName()});
+
+        //when
+        Collection<TestSelection> testSelection =
+            new CategorizedTestsDetector(config).selectTestsFromClasses(Arrays.asList(FirstCategorizedClass.class,
+                FirstAndSecondCategorizedClass.class));
+        //then
+        assertThat(testSelection).containsTestClassSelectionsExactlyInAnyOrder(getTestSelection(FirstCategorizedClass.class));
     }
 
     @Test
@@ -133,24 +169,6 @@ public class CategorizedTestsDetectorTest {
             .containsTestClassSelectionsExactlyInAnyOrder(
                 getTestSelection(FastTaggedClass.class),
                 testSelectionWithMethods);
-
-    }
-
-    @Test
-    public void should_return_class_and_methods_with_both_first_and_second_category_using_match_all() {
-        // given
-        categorizedConfig.setCategories(
-            new String[] {FirstCategory.class.getSimpleName(), SecondCategory.class.getSimpleName().toLowerCase()});
-        categorizedConfig.setMatchAll(true);
-
-        // when
-        Collection<TestSelection> testSelection =
-            new CategorizedTestsDetector(config).selectTestsFromClasses(classesToProcess);
-
-        // then
-        assertThat(testSelection)
-            .containsTestClassSelectionsExactlyInAnyOrder(
-                getTestSelection(FirstAndSecondCategorizedClass.class));
     }
 
     @Test
@@ -174,43 +192,86 @@ public class CategorizedTestsDetectorTest {
     }
 
     @Test
-    public void should_return_all_classes_and_methods_with_any_category_when_no_category_is_set() {
-        // when
-        Collection<TestSelection> testSelection =
-            new CategorizedTestsDetector(config).selectTestsFromClasses(classesToProcess);
+    public void should_return_methods_without_excluded_category() {
+        //given
+        categorizedConfig.setCategories(new String[] {Included.class.getSimpleName()});
+        categorizedConfig.setExcludedCategories(new String[] {Excluded.class.getSimpleName()});
 
-        // then
+        //when
+        Collection<TestSelection> testSelection =
+            new CategorizedTestsDetector(config).selectTestsFromClasses(
+                Collections.singleton(WithExcludedMethodClass.class));
+
+        //then
         TestSelection testSelectionWithMethods =
-            getTestSelectionWithMethods(WithCategorizedMethodsClass.class, "testWithFirstCategory",
-                "testWithSecondCategory", "testWithThirdCategory");
-        assertThat(testSelection)
-            .containsTestClassSelectionsExactlyInAnyOrder(
-                getTestSelection(ThirdCategorizedClass.class),
-                getTestSelection(FirstAndSecondCategorizedClass.class),
-                getTestSelection(FirstCategorizedClass.class),
-                testSelectionWithMethods);
+            getTestSelectionWithMethods(WithExcludedMethodClass.class, "testWithoutExcludedCategory");
+
+        assertThat(testSelection).containsTestClassSelectionsExactlyInAnyOrder(testSelectionWithMethods);
     }
 
     @Test
-    public void should_return_classes_and_methods_without_first_category_when_reversed_is_used() {
-        // given
-        categorizedConfig.setCategories(
-            new String[] {FirstCategory.class.getSimpleName()});
-        categorizedConfig.setReversed(true);
+    public void should_return_methods_without_excluded_tag() {
+        //given
+        categorizedConfig.setCategories(new String[] {"included"});
+        categorizedConfig.setExcludedCategories(new String[] {"excluded"});
 
-        // when
+        //when
         Collection<TestSelection> testSelection =
-            new CategorizedTestsDetector(config).selectTestsFromClasses(classesToProcess);
+            new CategorizedTestsDetector(config).selectTestsFromClasses(
+                Collections.singleton(WithExcludedTagClass.class));
 
-        // then
+        //then
         TestSelection testSelectionWithMethods =
-            getTestSelectionWithMethods(WithCategorizedMethodsClass.class, "testWithSecondCategory",
-                "testWithThirdCategory", "testWithoutCategory");
-        assertThat(testSelection)
-            .containsTestClassSelectionsExactlyInAnyOrder(
-                getTestSelection(ThirdCategorizedClass.class),
-                getTestSelection(NonCategorizedClass.class),
-                testSelectionWithMethods);
+            getTestSelectionWithMethods(WithExcludedTagClass.class, "includedMethod");
+
+        assertThat(testSelection).containsTestClassSelectionsExactlyInAnyOrder(testSelectionWithMethods);
+    }
+
+    @Test
+    public void should_return_class_when_no_methods_are_matched_to_be_excluded() {
+        //given
+        categorizedConfig.setCategories(new String[] {FirstCategory.class.getSimpleName()});
+        categorizedConfig.setExcludedCategories(new String[] {Excluded.class.getSimpleName()});
+
+        //when
+        Collection<TestSelection> testSelection =
+            new CategorizedTestsDetector(config).selectTestsFromClasses(
+                Collections.singleton(FirstCategorizedClass.class));
+
+        assertThat(testSelection).containsTestClassSelectionsExactlyInAnyOrder(
+            getTestSelection(FirstCategorizedClass.class));
+    }
+
+    @Test
+    public void should_exclude_test_class_when_excluding_single_category_of_many() {
+        //given
+        categorizedConfig.setCategories(new String[] {FirstCategory.class.getSimpleName()});
+        categorizedConfig.setExcludedCategories(new String[] {SecondCategory.class.getSimpleName()});
+
+        //when
+        Collection<TestSelection> testSelection =
+            new CategorizedTestsDetector(config).selectTestsFromClasses(
+                Collections.singleton(FirstAndSecondCategorizedClass.class));
+
+        Assert.assertTrue(testSelection.isEmpty());
+    }
+
+    @Test
+    public void should_exclude_methods_when_matched_and_class_is_not_annotated() {
+        //given
+        categorizedConfig.setCategories(new String[] {FirstCategory.class.getSimpleName()});
+        categorizedConfig.setExcludedCategories(
+            new String[] {SecondCategory.class.getSimpleName(), ThirdCategory.class.getSimpleName()});
+
+        //when
+        Collection<TestSelection> testSelection =
+            new CategorizedTestsDetector(config).selectTestsFromClasses(
+                Collections.singleton(WithIncludedAndExcludedCategoriesOnMethodsClass.class));
+
+        TestSelection testSelectionWithMethods =
+            getTestSelectionWithMethods(WithIncludedAndExcludedCategoriesOnMethodsClass.class, "testWithFirstCategory");
+
+        assertThat(testSelection).containsTestClassSelectionsExactlyInAnyOrder(testSelectionWithMethods);
     }
 
     private TestSelection getTestSelection(Class clazz) {
